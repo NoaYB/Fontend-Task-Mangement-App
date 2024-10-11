ef63a8a626d180f5d242f5d2268138a27ef41a30
NoaYB <120508949+NoaYB@users.noreply.github.com>
2024-10-11 15:18:43 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // ודא שהייבוא נכון
-import Register from './components/Register';
-import Login from './components/Login';
-import TaskList from './components/TaskList';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import TaskPage from './pages/TaskPage';
+import LoginForm from './pages/LoginForm';
+import RegisterForm from './pages/RegisterForm';
 
 const App = () => {
+    console.log("testing");
     return (
         <Router>
-            <Routes> {/* Here we add tasklist */}
-                <Route path="/register" element={<Register />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/tasks" element={<TaskList />} />
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginForm/>} />
+                <Route path="/register" element={<RegisterForm/>} />
+                <Route path="/tasks" element={<TaskPage />} />
             </Routes>
         </Router>
     );
